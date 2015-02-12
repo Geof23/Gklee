df033c0f81b17b4b8528c781f5f6b62d74b26215
sawaya <sawaya@fractus.cs.utah.edu>
2015-02-12 14:55:07 -0700

    tweaked the main test file a bit so that it doesn't use the 'KLEE_HOME_DIR' enviro; instead it uses relative paths

--- tests/execute_test.sh
+++ tests/execute_test.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-echo "compiling with nvcc"
-nvcc -I $KLEE_HOME_DIR/Gklee/include $1 &> nvcc_log.txt
 
-echo "running a.out"
-./a.out &> a_out_log.txt
+THIS_HOME=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+if [ -e `which nvcc` ] 
+then
+		echo "compiling with nvcc"
+		nvcc -I $THIS_HOME/../Gklee/include $1 &> nvcc_log.txt
+
+		echo "running a.out"
+		./a.out &> a_out_log.txt
+fi
 
 echo "compiling with gklee-nvcc"
 options=""
@@ -11,13 +17,13 @@ if [  -e options.txt ]
 then
     options=`head -1 options.txt | tr -d '\n'`
 fi
-gklee-nvcc -I $KLEE_HOME_DIR/Gklee/include $options  $1 &> gklee_nvcc_log.txt
+$THIS_HOME/../bin/gklee-nvcc -I $THIS_HOME/../Gklee/include $options  $1 &> gklee_nvcc_log.txt
 
 echo "running with gklee" 
-(time gklee -max-time=3600 -max-memory=2048 ${1:0:-3}) &> gklee_log.txt
+(time $THIS_HOME/../bin/gklee -max-time=3600 -max-memory=2048 ${1:0:-3}) &> gklee_log.txt
 
 echo "running with gkleep"
-(time gklee -symbolic-config -max-time=3600 -max-memory=2048 ${1:0: -3}) &> gkleep_log.txt
+(time $THIS_HOME/../bin/gklee -symbolic-config -max-time=3600 -max-memory=2048 ${1:0: -3}) &> gkleep_log.txt
 
 echo "verifying output"
 ../verify.py &> results.txt
