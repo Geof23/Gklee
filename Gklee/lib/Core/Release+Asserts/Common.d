be70186256e9562e97fcda0663655e92e7121831
Geof <fredericflintstone@gmail.com>
2015-04-15 22:44:44 -0600

    doing cleanup of obsolete build directories

--- Gklee/lib/Core/Release+Asserts/Common.d
+++
@@ -1,5 +0,0 @@
-/home/peng/GITHUB/Gklee/Gklee/lib/Core/Release+Asserts/Common.o \
-  /home/peng/GITHUB/Gklee/Gklee/lib/Core/Release+Asserts/Common.d: \
-  Common.cpp /home/peng/GITHUB/Gklee/Gklee/lib/Core/Common.h
-
-/home/peng/GITHUB/Gklee/Gklee/lib/Core/Common.h:
