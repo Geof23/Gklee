ae344c4b6f34226b384b1043b8266be4e3a1e255
Geof <fredericflintstone@gmail.com>
2015-04-27 15:04:24 -0600

    Adding CI base test results

--- tests/clean.sh
+++ tests/clean.sh
@@ -1,3 +1,3 @@
  #!/bin/bash
-find . -type f -not -name "*.cu" -not -name "*.sh" -not -name "*.py" -not -name "expected.txt" -not -name "README" -not -name "options.txt" | xargs rm -fv
+find . -type f -not -name "*.cu" -not -name "*.sh" -not -name "*.py" -not -name "expected.txt" -not -name "README" -not -name "options.txt" -not -name 'canonical_results.txt" | xargs rm -fv
 rm -vrf */klee*
