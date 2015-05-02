4c299de5e818e565db5f38768069f1ce28612865
Geof <fredericflintstone@gmail.com>
2015-05-01 18:12:52 -0600

    removed tests from Gklee -- created new repo for the tests.

--- tests/clean.sh
+++
@@ -1,3 +0,0 @@
- #!/bin/bash
-find . -type f -not -name "*.cu" -not -name "*.sh" -not -name "*.py" -not -name "expected.txt" -not -name "README" -not -name "options.txt" -not -name 'canonical_results.txt" | xargs rm -fv
-rm -vrf */klee*
